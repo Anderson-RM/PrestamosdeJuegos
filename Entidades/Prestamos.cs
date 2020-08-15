aa48216e43d587d0bef36f49dbfffb7f79de1935
Anderson-RM <anderson_rodriguez@ucne.edu.do>
2020-08-15 01:28:12 -0400

    Se creo la entidad Prestamos

--- Entidades/Prestamos.cs
+++ Entidades/Prestamos.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace PrestamosdeJuegos.Entidades
 {
-    class Prestamos
+    public class Prestamos
     {
+        [Key]
+        public int PrestamoId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public int AmigoId { get; set; }
+        public string Observacion { get; set; }
+        public int CantidadJuegos { get; set; }
     }
 }
